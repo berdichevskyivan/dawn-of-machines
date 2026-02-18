c9d097006e0bf39632c5c2d9f1eb0cbe230f2ff4
Ivan Berdichevsky <berdichevskyivan@gmail.com>
2026-02-17 21:51:04 -0300

    Added calculateSight and mainInterval

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,49 @@ const io = new Server(server, {
 });
 
 const games = []; // this array contains all the currently active games
+const boardTemplate = Array.from({length: 100}, (_, i) => ({ 
+                                    id: i,
+                                    x: i % 10,
+                                    z: Math.floor(i / 10),
+                                    resource: null,
+                                    unit: null,
+                                    building: null,
+                                }))
+
+const calculateSight = (x, z) => {
+    // returns an array of integers, the id of the tiles
+    const sightArray = [];
+    // this is where we are right now
+    sightArray.push({ x, z })
+    // this is right
+    sightArray.push({ x:x+1, z })
+    // this is left
+    sightArray.push({ x:x-1, z })
+    // this is top
+    sightArray.push({ x, z:z+1 })
+    // this is bottom
+    sightArray.push({ x, z:z-1 })
+    // this is top-right
+    sightArray.push({ x:x+1, z:z+1 })
+    // this is top-left
+    sightArray.push({ x:x-1, z:z+1 })
+    // this is bottom-right
+    sightArray.push({ x:x+1, z:z-1 })
+    // this is bottom-left
+    sightArray.push({ x:x-1, z:z-1 })
+
+    console.log('sightArray: ', sightArray);
+
+    const sightTileArray = sightArray.map(coordinate => {
+        const tile = boardTemplate.find(tile=>tile.x === coordinate.x && tile.z === coordinate.z);
+        console.log('tile is: ', tile);
+        return tile?.id;
+    }).filter(id => id !== undefined)
+
+    console.log('sightTileArray is: ', sightTileArray);
+
+    return sightTileArray;
+}
 
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -81,6 +124,9 @@ io.on('connection', (socket) => {
                     id: 0,
                     name: 'Gather Node', // It can do everything, but its SPECIALIZED in gathering, so its throughput is MAX when gathering, as opposed to other nodes
                     player: socket.id, // IMPORTANT: if player is null, it means its NOT controlled by ANY player.
+                    sight: [],
+                    x: 0,
+                    z: 0,
                 }
             ], // TODO: add coordinates
             buildings: [
@@ -88,16 +134,53 @@ io.on('connection', (socket) => {
                     id: 0,
                     name: 'Assembly Plant',
                     player: socket.id,
+                    sight: [],
+                    x: 1,
+                    z: 1,
                 },
                 {
                     id: 1,
                     name: 'Generator',
                     player: socket.id,
+                    sight: [],
+                    x: 2,
+                    z: 2,
                 }
             ],
         };
 
         // here, we add resources, and also run functions and calculations to populate the board, place the players, place the units, etc
+
+        // we calculate the sight of the units
+        game.units = game.units.map(u => {
+            return {
+                ...u,
+                sight: calculateSight(u.x, u.z),
+            }
+        })
+
+        // we calculate the sight of buildings
+        game.buildings = game.buildings.map(b => {
+            return {
+                ...b,
+                sight: calculateSight(b.x, b.z),
+            }
+        })
+
+        // here, we also add the actions that we want starting ALREADY, as we said everything is an action
+        // sight: action; movement: action; drain-electricity: action;
+        // the interval checks the clock and passes the task to the resolver
+        // once the resolver is done with the task, it updates the state of it
+        // the interval can remove the action if: 1) timeToResolve reached the limit 2) the action is "resolved"
+        // for example, we need to calculate SIGHT for each unit. SIGHT will tell us which TILE they see. 
+        // the Tile they see is calculated based on the X and Z of the object (unit or building) like x+1 x-1 z+1 z-1, then that matches an ID, THEN that id is pushed into an array called `sight` inside
+        // the unit of building
+        const mainInterval = setInterval(()=>{
+            console.log('to test');
+        }, 50)
+
+        // push the main interval into the intervals array
+        game.intervals.push(mainInterval);
 
         // start main interval. Each 50ms. resolves or removes (resolver doest the ACTUAL change, interval checks time and determines if enough has passed. Thats it.)
         // store than main interval id somewhere for cleaning afterwards. The interval sends state changes to the player but ONLY the player data
@@ -108,9 +191,12 @@ io.on('connection', (socket) => {
         // we tell all clients a game has started can be viewed in /games page
         io.emit('games-update', games.map(g => ({title: g.title, startingTime: g.startingTime})));
 
-        // we give the player that started the necesary data to start the game
-        // later we differentiate the data we can send or not, for now, it can be cleanly sent
-        socket.emit('starting-game-data', game);
+        // Neccesary data to start the board
+        // Remove intervals from client side data
+        const safeGameData = { ...game };
+        delete safeGameData.intervals;
+
+        socket.emit('starting-game-data', safeGameData);
     })
 })
 
